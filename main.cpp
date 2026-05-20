533c9dcaffa4b5895788011254e7a8d1cffa2b4b
Your Name <brightsy04@gmail.com>
2026-05-20 03:09:42 -0700

    30min-milestone1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Milestone1
 #include <iostream>
 #include <string>
 
@@ -14,5 +15,30 @@ int main()
 {
     srand(time(0));
 
-    string names[] = {"Seoyeon", "James", "Seongjun", "Alyssa", "Dayeon"}
+    string names[] = {"Seoyeon", "James", "Seongjun", "Alyssa", "Dayeon"};
+    string drinks[] = {"Latte", "Mocha", "Americano", "Cappuccino", "Cold Brew", "Espresso"};
+
+    CoffeeNode* head = nullptr;
+    CoffeeNode* tail = nullptr;
+  
+
+    for (int i = 0; i < 3; i++)
+    {
+        CoffeeNode* newNode = new CoffeeNode;
+
+        newNode->name = names[rand() % 6];
+        newNode->drink = drinks[rand() % 6];
+        newNode->next = nullptr;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
 }
