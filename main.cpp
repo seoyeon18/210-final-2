d39808cea74369db3b623812acd465a267685f14
Your Name <brightsy04@gmail.com>
2026-05-20 03:01:58 -0700

    20min-milestone1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
-//thinking,,
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct CoffeeNode
+{
+    string name;
+    string drink;
+    CoffeeNode* next;
+};
+
+int main()
+{
+    srand(time(0));
+
+    string names[] = {"Seoyeon", "James", "Seongjun", "Alyssa", "Dayeon"}
+}
